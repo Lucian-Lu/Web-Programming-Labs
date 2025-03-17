6509a9ae948a66393931526d626d27f66510e7fd
Lucian-Lu <lupanlucian4@gmail.com>
2025-03-17 20:23:10 +0200

    Add web engine search

--- lab5.py
+++ lab5.py
@@ -1,6 +1,17 @@
 import socket, ssl
 from bs4 import BeautifulSoup
 
+
+# Headers to prevent getting the bot verification redirect
+headers_get = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:102.0) Gecko/20100101 Firefox/102.0',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'Accept-Language': 'en-US,en;q=0.5',
+    'DNT': '1',
+    'Connection': 'close'
+}
+
+
 def send_https_request(host, path):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)   
     # Wrapping the socket for HTTPS connection
@@ -32,16 +43,50 @@ def return_help_info():
     # Return help info
     return "go2web -u <URL>         # make an HTTP request to the specified URL and print the response\ngo2web -s <search-term> # make an HTTP request to search the term using your favorite search engine and print top 10 results\ngo2web -h               # show this help"
 
-def send_web_engine_request():
-    # Dummy function
-    return "WIP"
 
-# host = "protv.md"
-# path = "/"
+# Function to build the https request data
+def build_headers(path, host):
+    request_line = f"GET {path} HTTP/1.1\r\n"
+    header_lines = f"Host: {host}\r\n"
+    for key, value in headers_get.items():
+        header_lines += f"{key}: {value}\r\n"
+    header_lines += "\r\n"
+    return request_line + header_lines
 
-# html_body = send_https_request(host, path)
-# print(html_body)
 
+# Function that will use duckduckgo html search to return page data
+def send_web_engine_request(query):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s_socket = ssl.wrap_socket(sock)
+    query = '+'.join(query.split())
+    host = 'html.duckduckgo.com'
+    path = '/html?q=' + query
+    request_data = build_headers(path, host)
+    
+    try:
+        s_socket.connect((host, 443))
+        s_socket.sendall(request_data.encode())
+        response = b""
+        while True:
+            chunk = s_socket.recv(4096)
+            if not chunk:
+                break
+            response += chunk
+        response_str = response.decode('utf-8', errors='replace')
+        header, _, body = response_str.partition("\r\n\r\n")
+    finally:
+        s_socket.close()
+
+    soup = BeautifulSoup(body, "html.parser")
+    output = []
+
+    for result in soup.find_all('a', {'class': 'result__a'}):
+        title = result.get_text(strip=True)
+        url = result.get('href')
+        output.append({'title': title, 'url': url})
+    return output
+
+    
 if __name__ == "__main__":
     while True:
         command = input("Enter a command: ")
