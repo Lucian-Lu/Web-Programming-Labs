052bb4e4a45a44f2a63cdfd7444f97070074b5f0
Lucian-Lu <lupanlucian4@gmail.com>
2025-03-17 22:09:11 +0200

    Make responses human readable & improve compatiblity with websites

--- lab5.py
+++ lab5.py
@@ -12,15 +12,26 @@ headers_get = {
 }
 
 
+# Function to build the https request data
+def build_headers(host, path):
+    request_line = f"GET {path} HTTP/1.1\r\n"
+    header_lines = f"Host: {host}\r\n"
+    for key, value in headers_get.items():
+        header_lines += f"{key}: {value}\r\n"
+    header_lines += "\r\n"
+    return request_line + header_lines
+
+
 def send_https_request(host, path):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)   
-    # Wrapping the socket for HTTPS connection
-    s_socket = ssl.wrap_socket(sock)
+    # Creating context to ensure compatibility with websites
+    context = ssl.create_default_context()
+    s_socket = context.wrap_socket(sock, server_hostname=host)
     
     try:
         # Connecting to the server on the https port
         s_socket.connect((host, 443))
-        request = f"GET {path} HTTP/1.1\r\nHost: {host}\r\nConnection: close\r\n\r\n"
+        request = build_headers(host, path)
         s_socket.sendall(request.encode())
         
         # Receive the response in chunks
@@ -44,16 +55,6 @@ def return_help_info():
     return "go2web -u <URL>         # make an HTTP request to the specified URL and print the response\ngo2web -s <search-term> # make an HTTP request to search the term using your favorite search engine and print top 10 results\ngo2web -h               # show this help"
 
 
-# Function to build the https request data
-def build_headers(path, host):
-    request_line = f"GET {path} HTTP/1.1\r\n"
-    header_lines = f"Host: {host}\r\n"
-    for key, value in headers_get.items():
-        header_lines += f"{key}: {value}\r\n"
-    header_lines += "\r\n"
-    return request_line + header_lines
-
-
 # Function that will use duckduckgo html search to return page data
 def send_web_engine_request(query):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -61,18 +62,20 @@ def send_web_engine_request(query):
     query = '+'.join(query.split())
     host = 'html.duckduckgo.com'
     path = '/html?q=' + query
-    request_data = build_headers(path, host)
+    request_data = build_headers(host, path)
     
     try:
         s_socket.connect((host, 443))
         s_socket.sendall(request_data.encode())
+
         response = b""
         while True:
             chunk = s_socket.recv(4096)
             if not chunk:
                 break
             response += chunk
-        response_str = response.decode('utf-8', errors='replace')
+
+        response_str = response.decode('utf-8')
         header, _, body = response_str.partition("\r\n\r\n")
     finally:
         s_socket.close()
@@ -80,15 +83,38 @@ def send_web_engine_request(query):
     soup = BeautifulSoup(body, "html.parser")
     output = []
 
+    print(body)
+    # Scrape page data from html file 
     for result in soup.find_all('a', {'class': 'result__a'}):
         title = result.get_text(strip=True)
         url = result.get('href')
         output.append({'title': title, 'url': url})
+
     return output
+
+
+def parse_html(html_body):
+    soup = BeautifulSoup(html_body, "html.parser")
+    sections = {}
+    
+    title_tag = soup.find("title")
+    if title_tag:
+        sections["title"] = title_tag.get_text()
+
+    i = 1
+    for p in soup.find_all("p"):
+        text = p.get_text()
+        if text:
+            sections[f"paragraph{i}"] = text
+        i += 1
+    
+    return sections
 
     
 if __name__ == "__main__":
     while True:
+        response = ""
+
         command = input("Enter a command: ")
         split_command = command.split(" ")
         try:
@@ -118,13 +144,17 @@ if __name__ == "__main__":
         if cmd_2 == "-u":
             try:
                 host, path = cmd_3.split("://")[-1].split("/", 1)
-                print(host, path)
+                path = "/" + path
+
                 response = send_https_request(host, path)
+                response = parse_html(response)
+                
                 print(response)
-            except:
-                print("Error occurred = ", response)
+            
+            except Exception as e:
+                print("Error occurred = ", e)
         elif cmd_2 == "-s":
-            response = send_web_engine_request()
+            response = send_web_engine_request(cmd_3)
             print(response)
         if cmd_2 == "-h":
             response = return_help_info()
